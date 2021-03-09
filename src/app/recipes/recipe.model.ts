b8536628d06733849fe74dfa9fc611fda84493e8
PlankelD <71640348+PlankelD@users.noreply.github.com>
2021-03-09 19:44:59 +0100

    Ingredients

--- src/app/recipes/recipe.model.ts
+++ src/app/recipes/recipe.model.ts
@@ -1,18 +1,15 @@
+import { Ingredient } from "../shared/ingredient.model";
+
 export class Recipe {
   public name: string;
   public description: string;
-  // auf den Pfad speichern, der auf das Bild verweist. bilder vom web -> also url - also string
   public imagePath: string;
+  public ingredients: Ingredient[];
 
-  //damit wir dies mit dem neuen Schlüsselwort instanziieren
-  // constructor selbst: ist einfach eine integrierte Funktion, die jede Klasse hat und die ausgeführt wird,sobald Sie eine neue
-  //                    Instanz dieser Klasse erstellen.
-  constructor(name: string, desc: string, imagePath: string) {
-       // Im body dieses Konstruktors muss ich die Argumente, die wir hier erhalten, jetzt den Eigenschaften unseres Objekts
-      // und den Eigenschaften unserer Klasse zuordnen
-
+  constructor(name: string, desc: string, imagePath: string, ingredients: Ingredient[]) {
       this.name = name;
       this.description = desc;
       this.imagePath = imagePath;
+      this.ingredients = ingredients;
   };
 };
--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,4 +1,6 @@
 import { Injectable, EventEmitter } from "@angular/core";
+import { Ingredient } from "../shared/ingredient.model";
+import { ShoppingListService } from "../shopping-list/shopping-list.service";
 import { Recipe } from "./recipe.model";
 
 @Injectable({
@@ -9,15 +11,31 @@ export class RecipeService {
 
 
  private recipes: Recipe[] = [
-    new Recipe('A Test Recipe', 'This is simply test', 'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg'),
-    new Recipe('Another Test Recipe', 'This is simply test', 'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg')
+    new Recipe(
+      'Tasty Schnitzel',
+      'A super-tasty Schnitzel - just awesome!',
+      'https://cdn.pixabay.com/photo/2016/11/19/02/22/schnipo-1837703_960_720.jpg',
+      [
+        new Ingredient('Meat', 1),
+        new Ingredient('French Fries', 20)
+      ]),
+    new Recipe(
+      'Big Fat Burger',
+      'What else you need to say',
+      'https://cdn.pixabay.com/photo/2016/03/05/19/02/hamburger-1238246_960_720.jpg',
+      [
+        new Ingredient('Buns', 2),
+        new Ingredient('Meat',2)
+      ])
   ];
 
-  // if i were to return it like this, i actually return the direct refernece to this array..well
-  // if we now change something on this array, we will change it on the array in this service
-  // slice() without value return a new arry which is an exact copy of the one in this service file
-  // we get a copy!!
+  constructor(private slService: ShoppingListService){}
+
   getRecipes(){
     return this.recipes.slice();
+  }
+
+  addIngredientsToShoppingList(ingredients: Ingredient[]){
+    this.slService.addIngredients(ingredients);
   }
 }
